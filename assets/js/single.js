592dcdb8cae323b515fa953f23747cc66462d1a5
dakota <kotalilyy@gmail.com>
2021-03-16 14:35:26 -0500

    add content to elements

--- assets/js/single.js
+++ assets/js/single.js
@@ -39,3 +39,27 @@ var displayIssues = function(issues) {
       issueEl.classList = "list-item flex-row justify-space-between align-center";
       issueEl.setAttribute("href", issues[i].html_url);
       issueEl.setAttribute("target", "_blank");
+
+      // create span to hold issue title
+    var titleEl = document.createElement("span");
+    titleEl.textContent = issues[i].title;
+    
+  
+    // append to container
+    issueEl.appendChild(titleEl);
+
+    // create a type element
+    var typeEl = document.createElement("span");
+
+    // check if issue is an actual issue or a pull request
+    if (issues[i].pull_request) {
+      typeEl.textContent = "(Pull request)";
+    }
+    else {
+      typeEl.textContent = "(Issue)";
+    }
+
+    // append to container
+    issueEl.appendChild(typeEl);
+
+    
